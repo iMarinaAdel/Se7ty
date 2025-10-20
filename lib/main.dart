33b6af256302ff5a3fb458a648920766b5b85a41
Marina Adel <rinaa1775@gmail.com>
2025-10-20 10:30:47 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Se7ty());
+}
+
+class Se7ty extends StatelessWidget {
+  const Se7ty({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // home: ,
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
